2da2ef245261f67db68c90ae952a16f4c4088437
ankurdotio <prajapatiankur8349@gmail.com>
2026-03-08 17:33:43 +0530

    first commit

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as authController from "../controllers/auth.controller.js";
+
+
+const authRouter = Router();
+
+
+
+/**
+ * POST /api/auth/register
+ */
+authRouter.post("/register", authController.register)
+
+
+export default authRouter;
